d63925931e2df296360b37ca5548dfede3915097
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-07-18 20:26:43 -0300

    Form updating employees

--- src/main/java/com/example/spring_mvc_crud/controller/EmployeeController.java
+++ src/main/java/com/example/spring_mvc_crud/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.spring_mvc_crud.entity.Employee;
 import com.example.spring_mvc_crud.service.EmployeeService;
@@ -50,5 +51,14 @@ public class EmployeeController {
 		service.save(employee);
 		
 		return "redirect:/employees/list";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId")int id, Model model) {
+		Employee employee = service.findById(id);
+		
+		model.addAttribute("employee", employee);
+		
+		return "employees/employee-form";
 	}
 }
