9615ae65173984e2e482690fdd8fea8f1477d57b
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-07-18 11:01:53 -0300

    Created controller layer

---
+++ src/main/java/com/example/spring_mvc_crud/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.example.spring_mvc_crud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.spring_mvc_crud.entity.Employee;
+import com.example.spring_mvc_crud.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService service;
+
+	@Autowired
+	public EmployeeController(EmployeeService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/list")
+	public String listEmployees(Model model) {
+		
+		List<Employee> employees = service.findAll();
+		
+		model.addAttribute("employees", employees);
+		
+		return "list-employees";
+	}
+
+}
