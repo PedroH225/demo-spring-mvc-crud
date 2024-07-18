de9a2d2db522433f89b1a144fae29fb72fc79b34
Pedro Henrique <pedrodguimaraes20@gmail.com>
2024-07-18 10:42:48 -0300

    Project created

---
+++ src/main/java/com/example/spring_mvc_crud/DemoSpringMvcCrud.java
@@ -0,0 +1,13 @@
+package com.example.spring_mvc_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringMvcCrud {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringMvcCrud.class, args);
+	}
+
+}
